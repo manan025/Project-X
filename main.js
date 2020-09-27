e8451f00039e29f3fa716b58e70201d95dd4a113
testb-35 <yigitakin3551@gmail.com>
2020-09-27 14:45:32 +0300

    demo completed!

--- main.js
+++ main.js
@@ -8,11 +8,36 @@ const firebaseConfig = {
   appId: "1:468663955049:web:f79f2110d2c136cfb57cf8",
   measurementId: "G-PPQPJYHPKM",
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 let db = firebase.database();
+
+postsRef = db.ref("posts/");
+
+//retrieving data
+
+postsRef.on("value", gotData, gotError);
+
+function gotData(data) {
+  var dataSet = data.val();
+  var dataKeys = Object.keys(dataSet);
+
+  for (var i = 0; i < dataKeys.length; i++) {
+    var dataKey = dataKeys[i];
+
+    var title = dataSet[dataKey].postTitle;
+    var content = dataSet[dataKey].content;
+    var countryTag = dataSet[dataKey].postCountryTag;
+    var nameTag = dataSet[dataKey].postNameTag;
+  }
+}
+
+function gotError(error) {
+  console.log(error);
+}
 
 //grab form element
 const form = document.querySelector("form");
@@ -22,15 +47,17 @@ form.onsubmit = (e) => {
   e.preventDefault();
   const formObj = new FormData(form);
   const title = formObj.get("title");
-  const tags = formObj.get("tag");
+  const country_tag = formObj.get("country_tag");
+  const name_tag = formObj.get("name_tag");
   const content = formObj.get("content");
 
   //to-do: add userID dynamically
-  rootRef = db.ref("posts/");
-  rootRef.push({
+  postsRef.push({
     postTitle: title,
-    postTags: tags,
+    postNameTag: name_tag,
+    postCountryTag: country_tag,
     content: content,
   });
   form.reset();
-};
+}
+
